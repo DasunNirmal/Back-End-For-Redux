74e64f9db12410e2197fdd8af5e113503d3a5f7d
DasunNirmal <dasun200301@gmail.com>
2025-01-30 23:13:58 +0530

    updated to handle cors and customer context path is added

--- server.ts
+++ server.ts
@@ -1,6 +1,18 @@
 import express from 'express';
+import customerRoutes from "./routes/customer-routes";
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use('/customer',customerRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
