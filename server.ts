2e92d4d5425dd04bf4b6c66a27835964aa4188ac
DasunNirmal <dasun200301@gmail.com>
2025-01-31 01:13:57 +0530

    updated

--- server.ts
+++ server.ts
@@ -1,19 +1,20 @@
 import express from 'express';
+import cors from 'cors';
 import customerRoutes from "./routes/customer-routes";
+import itemRoutes from "./routes/item-routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+app.use(cors({
+    origin: "http://localhost:5173",  // Allow frontend requests
+    methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept"
+}));
 
 app.use('/customer',customerRoutes);
-app.use('/item');
+app.use('/item',itemRoutes);
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
